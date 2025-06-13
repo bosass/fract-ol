d2b728086d03aa38ec7d8d70c454ac8ae272b252
Bohdan <81105398+bosass@users.noreply.github.com>
2025-06-13 13:39:37 +0200

    wow

---
+++ math_complex.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   math_complex.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bosokyrk <bosokyrk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/03 23:52:18 by bosokyrk          #+#    #+#             */
+/*   Updated: 2025/06/03 23:52:22 by bosokyrk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+t_complex	csqr(t_complex c)
+{
+	t_complex	out;
+
+	out.x = (c.x * c.x) - (c.y * c.y);
+	out.y = c.y * c.x * 2;
+	return (out);
+}
+
+t_complex	cadd(t_complex c1, t_complex c2)
+{
+	t_complex	out;
+
+	out.x = c1.x + c2.x;
+	out.y = c1.y + c2.y;
+	return (out);
+}
+
+double	absv(double n)
+{
+	if (n < 0)
+		return (-n);
+	return (n);
+}
+
+double	chyp(t_complex c)
+{
+	return (c.x * c.x + (c.y * c.y));
+}
